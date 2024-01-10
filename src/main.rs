f995fa3387d8c090798f860d82f551f6d7136b6c
Johnny Luo <johnnyluo1980@gmail.com>
2024-01-10 17:02:13 +1100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{middleware, web, App, HttpServer, HttpRequest};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    log::info!("starting HTTP server at http://localhost:8080");
+    HttpServer::new(|| {
+        App::new()
+            // enable logger
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/index.html").to(|| async { "Hello world!" }))
+            .service(web::resource("/").to(index))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+async fn index(req: HttpRequest) -> &'static str {
+    println!("REQ: {req:?}");
+    "Hello world!"
 }
